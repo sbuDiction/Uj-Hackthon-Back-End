f070bd70b7d37dc6a6f8bb3afd04b81ee6f87d0f
Natasia Mahure <paris.mahure@gmail.com>
2022-11-09 15:33:30 +0200

    insertdata

--- database/005-insert-data.sql
+++ database/005-insert-data.sql
@@ -1,1 +1,13 @@
-INSERT INTO user (id, name, role, team_leader, admin) VALUES (1, 'Thabiso Gama', 'Member', false);
+INSERT INTO user (id, name, role, team_leader, admin) VALUES (1, 'Thabiso Gama', 'Member', false, false);
+INSERT INTO user (id, name, role, team_leader, admin) VALUES (2, 'Mahure Natasia', 'Member', false, false);
+INSERT INTO user (id, name, role, team_leader, admin) VALUES (3, 'Mapaila Zoella', 'Member', false, false);
+INSERT INTO user (id, name, role, team_leader, admin) VALUES (4, 'Thabile Nkosi', 'Member', false, false);
+INSERT INTO user (id, name, role, team_leader, admin) VALUES (5, 'Sibusiso Nkosi', 'Member', false, false);
+
+-- team leaders
+INSERT INTO user (id, name, role, team_leader, admin) VALUES (6, 'Letlothlo Khoza', 'Team Leader', true, false);
+INSERT INTO user (id, name, role, team_leader, admin) VALUES (7, 'Thamsaqa Mabuza', 'Team Leader', true, false);
+
+--project manager 
+INSERT INTO user (id, name, role, team_leader, admin) VALUES (8, 'Thabisile Duma', 'Project Manager', false, true);
+INSERT INTO user (id, name, role, team_leader, admin) VALUES (9, 'Thandi Gama', 'Project Manager', false, true);
