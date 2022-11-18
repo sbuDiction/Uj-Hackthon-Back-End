ad33a4f029cf4334be0d5da09382e386b70cc716
sbuDiction <nsbu68501@gmail.com>
2022-11-18 11:31:06 +0200

    make it better

--- controllers/projectManagement/createProject.controller.js
+++ controllers/projectManagement/createProject.controller.js
@@ -1,12 +1,22 @@
 import { Project } from "../../models/project.model.js";
 
 export const createProject = async (req, res, next) => {
-    const { user_id, project_name, start_date, end_date } = req.body;
-
-    await Project.create(req.body).then(() => {
-        res.json({
-            status: 'Project created'
+    const { project_name } = req.body;
+    await Project.findOne({ where: { project_name: project_name } })
+        .then(async project => {
+            if (!project) await Project.create(req.body).then(() => {
+                res.json({
+                    message: 'Project has been successfuly created',
+                    isCreated: true,
+                    alertType: 'green-400'
+                })
+            })
+            else res.json({
+                message: 'Project already exists try creating a new project',
+                isCreated: false,
+                alertType: 'blue-400'
+            })
         })
-    })
+
 
 }
