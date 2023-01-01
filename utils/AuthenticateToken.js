0de292a69617b0a8ca793dc4f15c6e4330c991fc
sbuDiction <sibusisonkosi0897@gmail.com>
2023-01-01 16:10:28 +0200

    fixed issue

--- utils/AuthenticateToken.js
+++ utils/AuthenticateToken.js
@@ -1,5 +1,8 @@
 import jwt from 'jsonwebtoken';
-import config from '../config/config.json' assert {type: 'json'}
+import { createRequire } from "module"; // Bring in the ability to create the 'require' method
+const require = createRequire(import.meta.url); // construct the require method
+const my_json_file = require("../config/config.json") // use the require method
+// import config from '../config/config.json' assert {type: 'json'}
 
 
 export const AuthenticateToken = (token) => jwt.verify(token, config.TOKEN_LIFE);
--- utils/GenerateToken.js
+++ utils/GenerateToken.js
@@ -1,4 +1,6 @@
 import jwt from 'jsonwebtoken';
-import config from '../config/config.json' assert {type: 'json'}
+import { createRequire } from "module"; // Bring in the ability to create the 'require' method
+const require = createRequire(import.meta.url); // construct the require method
+const my_json_file = require("../config/config.json") // use the require method
 
 export const GenerateToken = (userId) => jwt.sign({ userId: userId }, config.TOKEN_LIFE, { expiresIn: config.TOKEN_LIFE });
